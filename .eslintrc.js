82bb7d5da5342595df40add32efbe55cb5f62039
Lasha Kava <lasharela@gmail.com>
2021-03-14 16:48:17 -0400

    Add modul resolver

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,23 @@
 module.exports = {
   root: true,
   extends: '@react-native-community',
+  settings: {
+    'import/resolver': {
+      node: {
+        paths: ['src'],
+        alias: {
+          '@context': './src/context',
+          '@components': './src/components',
+          '@graphql': './src/graphql',
+          '@navigation': './src/navigation',
+          '@services': './src/services',
+          '@styles': './src/styles',
+          '@store': './src/store',
+          '@utilities': './src/utilities',
+          '@views': './src/views',
+          '@hooks': './src/hooks',
+        },
+      },
+    },
+  },
 };
--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,23 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        extensions: ['.ts', '.ios.ts', '.android.ts', '.tsx'],
+        alias: {
+          '@components': './src/components',
+          '@screens': './src/screens',
+          '@navigation': '/navigation',
+          '@services': '/services',
+          '@styles': '/styles',
+          '@store': '/store',
+          '@utilities': '/utilities',
+          '@views': '/views',
+          '@hooks': '/hooks',
+        },
+      },
+    ],
+  ],
 };
