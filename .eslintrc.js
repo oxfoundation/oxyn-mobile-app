1d3326e7055fd16b47fe5f6fac82584c098b4ae2
Lasha Kava <lasharela@gmail.com>
2021-03-20 14:01:14 -0400

    Some modifications for ESLint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,11 @@
 module.exports = {
   root: true,
   extends: '@react-native-community',
+  plugins: ["import"],
+  rules: {
+    // turn on errors for missing imports
+    'import/no-unresolved': 'error',
+  },
   settings: {
     'import/parsers': {
       '@typescript-eslint/parser': ['.ts', '.tsx'],
